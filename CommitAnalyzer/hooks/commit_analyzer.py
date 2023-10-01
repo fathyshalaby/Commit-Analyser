5b1bcd6bff8a96ce24cdd9cac11a67bdac6555da
friends-in-flats <131982069+friends-in-flats@users.noreply.github.com>
2023-10-01 22:43:22 +0200

    init

--- CommitAnalyzer/hooks/commit_analyzer.py
+++ CommitAnalyzer/hooks/commit_analyzer.py
@@ -5,15 +5,12 @@ import re
 import os
 
 def main():
-    # Get the repository root
-    repo_root = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).decode('utf-8').strip()
-
-    # Load the desired regex pattern from .commitformat
-    format_file_path = os.path.join(repo_root, '.commitformat')
+    # Load the desired regex pattern from .commitformat in the current repo
+    format_file_path = '.commitformat'
     
     if not os.path.exists(format_file_path):
         print("Error: .commitformat file not found in the repository root!")
-        exit(1)
+        return 1
 
     with open(format_file_path, 'r') as f:
         pattern = f.read().strip()
@@ -25,7 +22,9 @@ def main():
     if not re.match(pattern, commit_msg):
         print(f"Error: Commit message does not match the required format specified in .commitformat!")
         print(f"Expected format (regex): {pattern}")
-        exit(1)
+        return 1
+
+    return 0
 
 if __name__ == "__main__":
-    main()
+    exit(main())
